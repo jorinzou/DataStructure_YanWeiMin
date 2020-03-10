9131e5253ba91d64ad334914b75766202cbdcc8e
jorinzou <2391387647@qq.com>
2020-03-10 22:23:49 +0800

    二分法查找

---
+++ DataStructure/algorithm/find/01binary_search.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+/*二分法查找，递归版本*/
+int binary_search(int arr[],int min_index,int max_index,int data)
+{
+	if(min_index <= max_index){
+		int mid = (max_index + min_index)/2;
+		
+		if (data == arr[mid]){
+			return mid;
+		}
+		else if (data < arr[mid]){
+			return binary_search(arr,min_index,mid-1,data);
+		}
+	    else{
+			return binary_search(arr,mid+1,max_index,data);
+		}
+	}
+	return -1;
+}
+
+int main(void)
+{
+	int arr[] = {0,1};
+	
+	int index = binary_search(arr,0,sizeof(arr)/sizeof(arr[0])-1,1);
+	if (index < 0){
+		printf("%d\n",index);
+	}else{
+		printf("%d\n",arr[index]);
+	}
+	return 0;
+}
